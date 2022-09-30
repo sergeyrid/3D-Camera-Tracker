264c54cfe815cbb2e04c4c37df7d04daefc66ec6
sergeyrid <sergeyrid@yandex.ru>
2022-09-30 23:51:49 +0300

    Add solution for first task

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -48,17 +48,43 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
+    max_corners = 250
+    quality_level = 0.01
+    min_distance = 15
     image_0 = frame_sequence[0]
+    corner_coords = cv2.goodFeaturesToTrack(image_0, max_corners, quality_level, min_distance)
+    corner_coords = corner_coords.reshape(-1, 2)
     corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+        np.arange(corner_coords.shape[0]),
+        corner_coords,
+        np.ones_like(corner_coords) * 30
     )
     builder.set_corners_at_frame(0, corners)
+    max_corners = 25
+    quality_level = 0.2
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
-        builder.set_corners_at_frame(frame, corners)
+        corner_coords, _, _ = cv2.calcOpticalFlowPyrLK(np.uint8(image_0 * 255), np.uint8(image_1 * 255),
+                                                       corner_coords, None,
+                                                       winSize=(15, 15), maxLevel=2,
+                                                       criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
+                                                                 10, 0.03))
+        if frame % 5 == 0 and corner_coords is not None:
+            mask = np.ones_like(image_1)
+            for coord in corner_coords:
+                mask = cv2.circle(mask, np.int8(coord), min_distance, 0, cv2.FILLED)
+            new_corner_coords = cv2.goodFeaturesToTrack(image_1, max(0, max_corners - corner_coords.shape[0]),
+                                                        quality_level, min_distance,
+                                                        mask=np.uint8(mask))
+            if new_corner_coords is not None:
+                new_corner_coords = new_corner_coords.reshape(-1, 2)
+            corner_coords = np.vstack((corner_coords, new_corner_coords))
+        corners = FrameCorners(
+            np.arange(corner_coords.shape[0]),
+            corner_coords,
+            np.ones_like(corner_coords) * 30
+        )
         image_0 = image_1
+        builder.set_corners_at_frame(frame, corners)
 
 
 def build(frame_sequence: pims.FramesSequence,
