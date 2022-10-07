83489696c292a63dbd849e9b983e1f5bcc017838
sergeyrid <sergeyrid@yandex.ru>
2022-10-08 01:02:45 +0300

    Add pyramids

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -16,6 +16,7 @@ import click
 import cv2
 import numpy as np
 import pims
+from copy import deepcopy
 
 from _corners import (
     FrameCorners,
@@ -56,65 +57,72 @@ class Corner:
         return self.quality > other.quality
 
 
-def _build_impl(frame_sequence: pims.FramesSequence,
-                builder: _CornerStorageBuilder) -> None:
-    image_0 = frame_sequence[0]
+def _build_pyr(image_0, image_1, corners_data, max_id, min_distance, mask):
     max_corners = 500
     quality_level = 0.03
-    min_distance = max(int(image_0.shape[0] / 50), 5)
 
-    mask = np.ones_like(image_0)
-    corner_coords, corner_qualities = cv2.goodFeaturesToTrackWithQuality(
-        image_0, max_corners, quality_level, min_distance, np.uint8(mask))
-    corner_coords = corner_coords.reshape(-1, 2)
-    corners_data = [Corner(i, corner_coords[i], corner_qualities[i]) for i in range(corner_coords.shape[0])]
-
-    corners = FrameCorners(
-        np.array(list(map(lambda corner: corner.id, corners_data))),
-        np.array(list(map(lambda corner: corner.position, corners_data))),
-        np.ones_like(corner_coords) * 2 * min_distance
-    )
-    builder.set_corners_at_frame(0, corners)
-
-    max_id = len(corners_data) - 1
-    for frame, image_1 in enumerate(frame_sequence[1:], 1):
+    if len(corners_data) > 0:
         corner_coords = np.array(list(map(lambda corner: corner.position, corners_data))).reshape(-1, 2)
         corner_coords, states, _ = cv2.calcOpticalFlowPyrLK(np.uint8(image_0 * 255), np.uint8(image_1 * 255),
                                                             corner_coords, None,
                                                             winSize=(15, 15), maxLevel=5,
                                                             criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
                                                                       10, 0.03))
+    else:
+        corner_coords = np.array([])
+        states = np.array([])
 
-        for i in range(len(corner_coords)):
-            corners_data[i].position = corner_coords[i]
-        corners_data = list(map(lambda pair: pair[0], filter(lambda pair: pair[1], zip(corners_data, states))))
-        corner_coords = np.array(list(map(lambda corner: corner.position, corners_data))).reshape(-1, 2)
+    for i in range(len(corner_coords)):
+        corners_data[i].position = corner_coords[i]
+    corners_data = list(map(lambda pair: pair[0], filter(lambda pair: pair[1], zip(corners_data, states))))
 
-        mask = np.uint8(np.ones_like(image_1))
-        for coord in corner_coords:
-            mask = cv2.circle(mask, (int(coord[0]), int(coord[1])), min_distance, 0.0, cv2.FILLED)
-        new_corner_coords, new_corner_qualities = cv2.goodFeaturesToTrackWithQuality(
-            image_1, max_corners, quality_level, min_distance, np.uint8(mask))
-        new_corners_data = []
-        if new_corner_coords is not None:
-            new_corner_coords = new_corner_coords.reshape(-1, 2)
-            new_corners_data = [Corner(max_id + i + 1, new_corner_coords[i], new_corner_qualities[i])
-                                for i in range(new_corner_coords.shape[0])]
-        max_id += len(new_corners_data)
+    new_corner_coords, new_corner_qualities = cv2.goodFeaturesToTrackWithQuality(
+        image_1, max_corners, quality_level, min_distance, np.uint8(mask))
+    new_corners_data = []
+    if new_corner_coords is not None:
+        new_corner_coords = new_corner_coords.reshape(-1, 2)
+        new_corners_data = [Corner(max_id + i + 1, new_corner_coords[i], new_corner_qualities[i])
+                            for i in range(new_corner_coords.shape[0])]
+    max_id += len(new_corners_data)
 
-        _, max_quality = cv2.goodFeaturesToTrackWithQuality(image_1, 1, 0.1, 0, np.uint8(np.ones_like(image_1)))
-        max_quality = max_quality[0]
-        new_corners_data = list(filter(lambda corner: corner.quality >= max_quality * quality_level,
-                                       new_corners_data))
-        corners_data += new_corners_data
+    _, max_quality = cv2.goodFeaturesToTrackWithQuality(image_1, 1, 0.1, 0, np.uint8(np.ones_like(image_1)))
+    max_quality = max_quality[0]
+    new_corners_data = list(filter(lambda corner: corner.quality >= max_quality * quality_level,
+                                   new_corners_data))
+    corners_data += new_corners_data
+    return deepcopy(corners_data), max_id
 
-        corners = FrameCorners(
-            np.array(list(map(lambda corner: corner.id, corners_data))),
-            np.array(list(map(lambda corner: corner.position, corners_data))),
-            np.ones_like(corners_data) * 2 * min_distance
-        )
-        image_0 = image_1
-        builder.set_corners_at_frame(frame, corners)
+
+def _build_impl(frame_sequence: pims.FramesSequence,
+                builder: _CornerStorageBuilder) -> None:
+    pyr_levels = 3
+    corners_data = [[] for _ in range(pyr_levels)]
+    image_0 = np.array([[]])
+    max_id = 0
+    circle_size = max(int(frame_sequence[0].shape[0] / 30), 5)
+    for frame, image in enumerate(frame_sequence[1:], 1):
+        corners = [[] for _ in range(3)]
+        image_1 = deepcopy(image)
+        mask = np.ones_like(image).astype(np.uint8)
+        for i in range(pyr_levels):
+            min_distance = max(int(image_1.shape[0] / 50), 5)
+            for j in range(pyr_levels):
+                corner_coords = np.array(list(map(lambda corner: corner.position, corners_data[j])))
+                for coord in corner_coords:
+                    mask = cv2.circle(mask, (int(coord[0] * 2**(j - i)), int(coord[1] * 2**(j - i))),
+                                      min_distance, 0.0, cv2.FILLED)
+            corners_data[i], max_id = _build_pyr(
+                image_0, image_1, deepcopy(corners_data[i]), max_id, min_distance, mask)
+            corners[0] += list(map(lambda corner: corner.id, corners_data[i]))
+            corners[1] += list(map(lambda corner: corner.position * 2**i, corners_data[i]))
+            corners[2] += list(np.ones_like(corners_data[i]) * circle_size)
+            mask = cv2.pyrDown(mask)
+            image_1 = cv2.pyrDown(image_1)
+            if image_0.shape[1] != 0:
+                image_0 = cv2.pyrDown(image_0)
+        builder.set_corners_at_frame(frame, FrameCorners(
+            np.array(corners[0]), np.array(corners[1]), np.array(corners[2])))
+        image_0 = deepcopy(image)
 
 
 def build(frame_sequence: pims.FramesSequence,
